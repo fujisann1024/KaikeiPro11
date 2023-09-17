8dd2baafd0addd4e8cd484489b8675fc5eea98dc
fujisann1024 <tehepero302@gmail.com>
2023-09-17 16:42:13 +0900

    初期ライブラリの追加

--- src/main/java/com/example/demo/KaikeiPro11Application.java
+++ src/main/java/com/example/demo/KaikeiPro11Application.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//アプリケーションクラス(アプリ名+Application)
 @SpringBootApplication
 public class KaikeiPro11Application {
 
 	public static void main(String[] args) {
+		//SpringBootを起動する
 		SpringApplication.run(KaikeiPro11Application.class, args);
 	}
 
