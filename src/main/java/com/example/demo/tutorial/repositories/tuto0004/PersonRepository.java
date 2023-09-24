1da435dfb5241500de7ec7884d2b4c72cec9e99b
fujisann1024 <tehepero302@gmail.com>
2023-09-25 00:56:20 +0900

    JpaRepositoryメソッドの実装例

--- src/main/java/com/example/demo/tutorial/repositories/tuto0004/PersonRepository.java
+++ src/main/java/com/example/demo/tutorial/repositories/tuto0004/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.tutorial.repositories.tuto0004;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,16 @@ import com.example.demo.tutorial.entity.tuto0004.Person;
 public interface PersonRepository extends JpaRepository<Person, String> {
 
 	public Optional<Person> findById(String name);
+	
+	//引数の値であいまい検索を行う※ワイルドカードは引数を渡す際に設定する
+	public List<Person> findByNameLike(String name);
+	
+	//IDがNULL以外かつIDを降順で取得
+	public List<Person> findByIdIsNotNullOrderByIdDesc();
+	
+	//ageの値が引数に指定した値よりも大きいものを検索
+	public List<Person> findByAgeGreaterThan(Integer aeg);
+	
+	//ageの値が指定の範囲内のものを検索する
+	public List<Person> findByAgeBetween(Integer age1, Integer age2);
 }
